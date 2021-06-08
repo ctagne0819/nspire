ba3773228199b53220147f75770bda03f55fde45
richlove45 <richlovenkansah@gmail.com>
2021-06-08 15:59:25 -0400

    pulling Chelsea's code with fixed css issue: background pic loads now.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, render_template, current_app as app
 from sense_hat import SenseHat
-#from flask_apscheduler import APScheduler
 import sqlite3
 
 app = Flask(__name__)
